a8acb223fbba57d575871bb775e5c9b8865650f7
Yooshin Kim <yusinkim.or@gmail.com>
2024-06-30 11:17:08 +0900

    feat : 구글 로그인 테스트

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,16 +1,51 @@
-import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { auth, provider, signInWithPopup } from "../firebase/firebase";
+import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
+import { useState } from "react";
 
 export default function LoginPage() {
-    const navigate = useNavigate();
-
-    function login() {
-        navigate("/home");
+    const [res, setRes] = useState("");
+    async function login() {
+        console.log("CLik!");
+        // try {
+        //     const result = await signInWithPopup(auth, provider);
+        //     const user = result.user;
+        //     const credential = GoogleAuthProvider.credentialFromResult(result);
+        //     if (credential) {
+        //         const token = credential.accessToken;
+        //         console.log("Access Token:", token);
+        //     }
+        //     console.log("User info:", user);
+        // } catch (error) {
+        //     console.error("Login error:", error);
+        // }
+        chrome.identity.getAuthToken({ interactive: true }, function (token) {
+            if (chrome.runtime.lastError) {
+                console.error("Error:", chrome.runtime.lastError.message);
+                return;
+            }
+            console.log("Token:", token);
+            // 토큰을 얻었을 때 수행할 추가적인 작업을 여기에 추가합니다.
+        });
     }
+
+    const loginWithGoogle = () => {
+        chrome.identity.getAuthToken({ interactive: true }, (token) => {
+            if (chrome.runtime.lastError || !token) {
+                chrome.tabs.create({
+                    url: "https://accounts.google.com/signin",
+                });
+                return;
+            }
+            console.log("signed in!", token);
+            setRes(token);
+        });
+    };
 
     return (
         <>
-            <Button onClick={login}>Login</Button>
+            <Button onClick={loginWithGoogle}>Login</Button>
+            <div>Token - {res}</div>
         </>
     );
 }
