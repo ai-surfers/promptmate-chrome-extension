0c3b912bf5e6083f230d5076949ef19d00e0da7d
Yooshin Kim <yusinkim.or@gmail.com>
2024-07-03 02:53:45 +0900

    refac : 테스트 코드 제거

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,30 +4,6 @@ import { useState } from "react";
 export default function LoginPage() {
     const [token, setToken] = useState("");
 
-    async function login() {
-        console.log("CLik!");
-        // try {
-        //     const result = await signInWithPopup(auth, provider);
-        //     const user = result.user;
-        //     const credential = GoogleAuthProvider.credentialFromResult(result);
-        //     if (credential) {
-        //         const token = credential.accessToken;
-        //         console.log("Access Token:", token);
-        //     }
-        //     console.log("User info:", user);
-        // } catch (error) {
-        //     console.error("Login error:", error);
-        // }
-        chrome.identity.getAuthToken({ interactive: true }, function (token) {
-            if (chrome.runtime.lastError) {
-                console.error("Error:", chrome.runtime.lastError.message);
-                return;
-            }
-            console.log("Token:", token);
-            // 토큰을 얻었을 때 수행할 추가적인 작업을 여기에 추가합니다.
-        });
-    }
-
     const loginWithGoogle = () => {
         chrome.identity.getAuthToken({ interactive: true }, (token) => {
             if (chrome.runtime.lastError || !token) {
