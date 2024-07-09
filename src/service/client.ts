c26b545206a227b6688bbd26f7a5a3d445471494
Yooshin Kim <yusinkim.or@gmail.com>
2024-07-09 21:30:22 +0900

    feat : axios inteceptor로 토큰 자동 삽입

--- src/service/client.ts
+++ src/service/client.ts
@@ -1,30 +1,37 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { getFromStorage } from "./chrome/storage";
+import { ACCESS_TOKEN } from "./chrome/storage.keys";
 
-interface BaseResponse<T> {
-    success: boolean;
-    detail: string;
-    data: T;
-}
+const getAccessToken = (): Promise<string> => {
+    return new Promise((resolve) => {
+        getFromStorage(ACCESS_TOKEN, (token) => {
+            console.log(">>", token);
+            resolve(token ? token : "");
+        });
+    });
+};
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
-
 const API = axios.create({
     baseURL: BASE_URL,
 });
 
 API.interceptors.request.use(
-    (config) => {
-        // const { accessToken } = useUserStore();
-
-        // if (accessToken) {
-        //     config.headers["Authorization"] = `Bearer ${accessToken}`;
-        // }
+    async (config) => {
+        const token = await getAccessToken();
+        config.headers["Authorization"] = `Bearer ${token}`;
         return config;
     },
     (error) => {
         return Promise.reject(error);
     }
 );
+
+interface BaseResponse<T> {
+    success: boolean;
+    detail: string;
+    data: T;
+}
 
 export const GET = async <T>(
     url: string,
