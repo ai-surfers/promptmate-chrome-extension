d1954829c0e5c700c7bf91c1b9f6d29f4aecd5e5
Yooshin Kim <yusinkim.or@gmail.com>
2024-07-15 20:40:17 +0900

    chore : 아이콘 기본 css 설정

--- public/content.js
+++ public/content.js
@@ -5,12 +5,19 @@ link.type = "text/css";
 link.href = chrome.runtime.getURL("style.css");
 document.head.appendChild(link);
 
-const button = document.createElement("button");
-button.id = "floatingButton";
-button.innerText = "+";
+// div 요소 생성
+const buttonContainer = document.createElement("div");
+buttonContainer.id = "float-btn";
 
-button.addEventListener("click", () => {
+// 이미지 요소 생성
+const img = document.createElement("img");
+img.src = chrome.runtime.getURL("images/logo.png"); // 이미지 경로 설정
+
+// 이미지를 div에 추가
+buttonContainer.appendChild(img);
+
+buttonContainer.addEventListener("click", () => {
     chrome.runtime.sendMessage({ action: "clickSidePanel" });
 });
 
-document.body.appendChild(button);
+document.body.appendChild(buttonContainer);
