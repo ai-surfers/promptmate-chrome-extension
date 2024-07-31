5e9359925813822c76e631b297547c0018a14f66
Yooshin Kim <yusinkim.or@gmail.com>
2024-07-31 17:50:57 +0900

    refac : 폼 사용하여 불필요한 useState 제거

--- src/components/newPrompt/PromptForm.tsx
+++ src/components/newPrompt/PromptForm.tsx
@@ -1,10 +1,7 @@
-import { useEffect, useMemo, useState } from "react";
-import { Button, Form } from "antd";
+import { useMemo } from "react";
+import { Button, Form, Input, Radio, Select } from "antd";
 import { Category, InputType, Visibility } from "../../core/Prompt";
-import ARadioGroup from "../common/input/ARadioGroup";
-import AInput from "../common/input/AInput";
-import ATextArea from "../common/input/ATextArea";
-import ASelectBox from "../common/input/ASelectBox";
+
 import { useForm } from "antd/es/form/Form";
 import { extractOptions } from "../../utils";
 import {
@@ -12,6 +9,7 @@ import {
     InputFormat,
 } from "../../hooks/mutations/prompt/usePostPrompt";
 import InputTags from "./PropertyForm/InputTags";
+import TextArea from "antd/es/input/TextArea";
 
 interface PromptFormProps {
     onSubmit: (promptData: CreatePromptRequest) => void;
@@ -20,21 +18,12 @@ interface PromptFormProps {
 export default function PromptForm({ onSubmit }: PromptFormProps) {
     const [form] = useForm();
 
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [category, setCategory] = useState<string[]>([]);
-    const [visibility, setVisibility] = useState(Visibility[0]);
-    const [prompt, setPrompt] = useState("");
-
+    const prompt = Form.useWatch("prompt", form);
     const inputs = useMemo(() => {
         return extractOptions(prompt);
     }, [prompt]);
 
-    useEffect(() => {
-        console.log("Prompt updated:", prompt);
-    }, [prompt]);
-
-    const handleOnFinish = () => {
+    const handleOnFinish = (values: CreatePromptRequest) => {
         const user_input_formats = inputs.map<InputFormat>((ip) => ({
             name: ip,
             type: InputType.TEXT,
@@ -42,24 +31,18 @@ export default function PromptForm({ onSubmit }: PromptFormProps) {
         }));
 
         const promptData: CreatePromptRequest = {
-            title: title,
-            description: description,
-            visibility: visibility,
-            categories: category,
-            prompt_template: prompt,
+            ...values,
             user_input_format: user_input_formats,
         };
+
+        console.log(">> promptData", promptData);
 
         onSubmit(promptData);
     };
 
     function insert(tag: string) {
-        console.log("tag! ", tag);
-        setPrompt((prevPrompt) => {
-            const newPrompt = `${prevPrompt} {{${tag}}}`;
-            console.log(")", newPrompt); // 최신 상태를 반영하여 로그
-            return newPrompt;
-        });
+        const newPrompt = `${prompt} {{${tag}}}`;
+        form.setFieldValue("prompt", newPrompt);
     }
 
     return (
@@ -69,54 +52,62 @@ export default function PromptForm({ onSubmit }: PromptFormProps) {
             form={form}
             onFinish={handleOnFinish}
         >
-            <Form.Item name="제목" label="제목" rules={[{ required: true }]}>
-                <AInput
-                    value={title}
-                    placeholder="마케팅 카피라이트 만들기"
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-            </Form.Item>
-
-            <Form.Item name="설명" label="설명" rules={[{ required: true }]}>
-                <AInput
-                    value={description}
-                    placeholder="마케팅 카피라이팅을 만드는 프롬프트"
-                    onChange={(e) => setDescription(e.target.value)}
-                />
+            <Form.Item name="title" label="제목" rules={[{ required: true }]}>
+                <Input placeholder="마케팅 카피라이트 만들기" />
             </Form.Item>
 
             <Form.Item
-                name="공개 범위"
+                name="description"
+                label="설명"
+                rules={[{ required: true }]}
+            >
+                <Input placeholder="마케팅 카피라이팅을 만드는 프롬프트" />
+            </Form.Item>
+
+            <Form.Item
+                name="visibility"
                 label="공개 범위"
                 rules={[{ required: true }]}
+                initialValue={Visibility[0]}
             >
-                <ARadioGroup
-                    value={visibility}
-                    options={Visibility}
-                    onChange={(vis) => setVisibility(vis.target.value)}
-                />
-            </Form.Item>
-
-            <Form.Item name="분야" label="분야" rules={[{ required: true }]}>
-                <ASelectBox
-                    options={Category}
-                    onChange={(cat) => setCategory(cat)}
-                    mode="tags"
-                />
+                <Radio.Group>
+                    {Visibility.map((opt) => (
+                        <Radio.Button value={opt} key={opt}>
+                            {opt}
+                        </Radio.Button>
+                    ))}
+                </Radio.Group>
             </Form.Item>
 
             <Form.Item
-                name="프롬프트"
+                name="categories"
+                label="분야"
+                rules={[{ required: true }]}
+            >
+                <Select
+                    placeholder="분야를 선택해주세요"
+                    allowClear
+                    mode="tags"
+                >
+                    {Category.map((cat) => (
+                        <Select.Option key={cat} value={cat}>
+                            {cat}
+                        </Select.Option>
+                    ))}
+                </Select>
+            </Form.Item>
+
+            <Form.Item
+                name="prompt"
                 label="프롬프트"
                 rules={[{ required: true }]}
-                valuePropName={prompt}
+                initialValue={""}
             >
-                <ATextArea
-                    value={prompt}
+                <TextArea
+                    style={{ minHeight: "100px" }}
                     placeholder="너는 마케팅 전문가야. $상품 이름$에 대한 마케팅 카피라이팅을 만들어줘. 예상 청중은 $예상 청중$이고 상품의 특징은 $상품 특징$.
 
 이들의 마음을 사로잡을 수 있는 매력적이고 센스있는 카피라이팅을 각기 다른 컨셉으로 총 3개 만들어줘"
-                    onChange={(e) => setPrompt(e.target.value)}
                 />
             </Form.Item>
 
