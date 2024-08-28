bc4b75ddb2e1b569b7871d7714773a0036ccac6b
Yooshin Kim <yusinkim.or@gmail.com>
2024-08-28 17:00:55 +0900

    refac : 타입 정의 추가

--- src/hooks/useFunnel.tsx
+++ src/hooks/useFunnel.tsx
@@ -1,30 +1,51 @@
-// useFunnel.tsx
-
+// [Reference] https://velog.io/@hoonnn/%ED%8D%BC%EB%84%90%EC%9D%84-%ED%86%B5%ED%95%9C-%EB%A7%8E%EC%9D%80-%ED%8E%98%EC%9D%B4%EC%A7%80-%EA%B4%80%EB%A6%AC%ED%95%98%EA%B8%B0-Feat.-useFunnel-React-Hook-Form
 import React, { ReactElement, ReactNode, useState } from "react";
 
-export interface StepProps {
-    name: string;
+export interface StepProps<T> {
+    name: T;
     children: ReactNode;
 }
 
-export interface FunnelProps {
-    children: Array<ReactElement<StepProps>>;
+export interface FunnelProps<T> {
+    children: Array<ReactElement<StepProps<T>>>;
 }
 
-export const useFunnel = (defaultStep: string) => {
-    const [step, setStep] = useState(defaultStep);
+export const useFunnel = <T extends string>(
+    steps: T[],
+    initialStepIndex: number = 0
+) => {
+    const [currentIndex, setCurrentIndex] = useState(initialStepIndex);
 
-    const Step = (props: StepProps): ReactElement => {
+    const Step = (props: StepProps<T>): ReactElement => {
         return <>{props.children}</>;
     };
 
-    const Funnel = ({ children }: FunnelProps) => {
+    const Funnel = ({ children }: FunnelProps<T>) => {
         const targetStep = children.find(
-            (childStep) => childStep.props.name === step
+            (childStep) => childStep.props.name === steps[currentIndex]
         );
 
         return <>{targetStep}</>;
     };
+    const nextStep = () => {
+        setCurrentIndex((prevIndex) =>
+            prevIndex < steps.length - 1 ? prevIndex + 1 : prevIndex
+        );
+    };
 
-    return { Funnel, Step, setStep, currentStep: step } as const;
+    const previousStep = () => {
+        setCurrentIndex((prevIndex) =>
+            prevIndex > 0 ? prevIndex - 1 : prevIndex
+        );
+    };
+
+    return {
+        Funnel,
+        Step,
+        nextStep,
+        previousStep,
+        setStepIndex: setCurrentIndex,
+        currentStep: steps[currentIndex],
+        currentIndex,
+    } as const;
 };
--- src/pages/tutorial/TutorialPage.tsx
+++ src/pages/tutorial/TutorialPage.tsx
@@ -12,35 +12,32 @@ enum TUTORIAL_STEPS {
     EXECUTE = "execute",
     FINISH = "finish",
 }
-
-type TUTORIAL_STEP_KEYS = keyof typeof TUTORIAL_STEPS;
-const STEPS: string[] = Object.values(TUTORIAL_STEPS);
+type TUTORIAL_STEP_VALUES =
+    (typeof TUTORIAL_STEPS)[keyof typeof TUTORIAL_STEPS];
+const steps = Object.values(TUTORIAL_STEPS);
 
 export default function TutorialPage() {
-    const { Funnel, Step, setStep, currentStep } = useFunnel(
-        TUTORIAL_STEPS.INTRO
-    );
+    const { Funnel, Step, nextStep, currentStep, currentIndex } =
+        useFunnel<TUTORIAL_STEP_VALUES>(steps, 0);
 
     return (
         <div>
-            tutorial {currentStep}
+            tutorial {currentStep} (Step {currentIndex + 1} of {steps.length})
             <Funnel>
                 <Step name={TUTORIAL_STEPS.INTRO}>
-                    <Tutorial1
-                        onNext={() => setStep(TUTORIAL_STEPS.PLATFORM)}
-                    />
+                    <Tutorial1 onNext={nextStep} />
                 </Step>
                 <Step name={TUTORIAL_STEPS.PLATFORM}>
-                    <Tutorial2 onNext={() => setStep(TUTORIAL_STEPS.INPUT)} />
+                    <Tutorial2 onNext={nextStep} />
                 </Step>
                 <Step name={TUTORIAL_STEPS.INPUT}>
-                    <Tutorial3 onNext={() => setStep(TUTORIAL_STEPS.EXECUTE)} />
+                    <Tutorial3 onNext={nextStep} />
                 </Step>
                 <Step name={TUTORIAL_STEPS.EXECUTE}>
-                    <Tutorial4 onNext={() => setStep(TUTORIAL_STEPS.FINISH)} />
+                    <Tutorial4 onNext={nextStep} />
                 </Step>
                 <Step name={TUTORIAL_STEPS.FINISH}>
-                    <Tutorial5 onNext={() => setStep(TUTORIAL_STEPS.FINISH)} />
+                    <Tutorial5 onNext={nextStep} />
                 </Step>
             </Funnel>
         </div>
