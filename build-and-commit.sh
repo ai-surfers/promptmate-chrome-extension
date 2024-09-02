543841b1781e2136c4fc96a1f7f1a0495998167e
Yooshin Kim <yusinkim.or@gmail.com>
2024-09-03 08:59:20 +0900

    [Hotfix/47-build] dev 빌드 스크립ㅌ 수정 (#49)
    * chore : env 파일 업데이트
    
    * Add build outputs
    
    * chore : build 스크립트 변경

--- build-and-commit.sh
+++ build-and-commit.sh
@@ -5,27 +5,18 @@
 # 종료 시점을 설정
 set -e
 
-# 환경 변수 파일 목록
-ENV_FILES=("ys" "jw")
-
 # 출력 디렉토리 생성
-for ENV_FILE in "${ENV_FILES[@]}"; do
-  echo "Building for environment: $ENV_FILE"
-  
-  # 출력 디렉토리 삭제 후 새로 생성
-  OUTPUT_DIR="outputs/$ENV_FILE"
-  rm -rf "$OUTPUT_DIR"
-  mkdir -p "$OUTPUT_DIR"
+OUTPUT_DIR="outputs"
+rm -rf "$OUTPUT_DIR"
+mkdir -p "$OUTPUT_DIR"
 
-  # 빌드 수행
-  ./node_modules/.bin/env-cmd -f promptmate-chrome-extension-envs/.env.development.$ENV_FILE yarn build:dev
+# 빌드 수행
+echo "Building using .env.development"
+./node_modules/.bin/env-cmd -f promptmate-chrome-extension-envs/.env.development yarn build:dev
 
-  # 빌드 결과물 이동
-  mv build/* outputs/$ENV_FILE/
-done
+# 빌드 결과물 이동
+mv build/* "$OUTPUT_DIR/"
 
 # 변경 사항 커밋
-git add outputs/
+git add "$OUTPUT_DIR/"
 git commit -m "Add build outputs"
-
-
---
+++ outputs/content.js
@@ -0,0 +1,59 @@
+/*global chrome */
+
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = chrome.runtime.getURL("style.css");
+document.head.appendChild(link);
+
+// Floating Button 추가
+const buttonContainer = document.createElement("div");
+buttonContainer.id = "float-btn";
+
+const img = document.createElement("img");
+img.src = chrome.runtime.getURL("images/logo.png");
+buttonContainer.appendChild(img);
+
+const span = document.createElement("span");
+span.innerText = "⌘P";
+buttonContainer.appendChild(span);
+
+buttonContainer.addEventListener("click", () => {
+    chrome.runtime.sendMessage({ action: "clickSidePanel" });
+});
+
+document.body.appendChild(buttonContainer);
+
+// // 버튼 강조, 해제 함수
+// function emphasizeButton() {
+//     console.log("emphasize!");
+//     const button = document.getElementById("float-btn");
+//     if (button) {
+//         button.classList.add("emphasized");
+
+//         // 툴팁 추가 (이미 있으면 추가하지 않음)
+//         if (!button.querySelector(".tooltip")) {
+//             const tooltip = document.createElement("div");
+//             tooltip.className = "tooltip";
+//             tooltip.textContent = "사이드 패널 열기";
+//             button.insertBefore(tooltip, button.firstChild);
+//         }
+//     }
+// }
+
+// function deemphasizeButton() {
+//     console.log("deemphasize!");
+//     const button = document.getElementById("float-btn");
+//     if (button) {
+//         button.classList.remove("emphasized");
+//         const tooltip = button.querySelector(".tooltip");
+//         if (tooltip) {
+//             tooltip.remove();
+//         }
+//     }
+// }
+
+// [메시지 수신 Listener]
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("***Message received", request, sender);
+});
