2693bfe52f64a4c69ce5e747a9dc7503dc05023f
Yooshin Kim <yusinkim.or@gmail.com>
2024-09-12 18:16:05 +0900

    chore : production 환경 clarity 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,31 @@ import AModal from "./components/common/modal/AModal";
 import Alert from "./components/common/alert/Alert";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./service/queryClient";
+import { useEffect } from "react";
 
 function App() {
     console.log(process.env.REACT_APP_MODE);
+
+    useEffect(() => {
+        if (process.env.REACT_APP_MODE === "production") {
+            console.log("* Clarity Added!");
+            (function (c: any, l: Document, a: string, r: string, i: string) {
+                c[a] =
+                    c[a] ||
+                    function () {
+                        (c[a].q = c[a].q || []).push(arguments);
+                    };
+                const t = l.createElement(r) as HTMLScriptElement;
+                t.async = true; // Use true instead of 1
+                t.src = "https://www.clarity.ms/tag/YOUR_CLARITY_ID";
+                const y = l.getElementsByTagName(r)[0];
+                if (y && y.parentNode) {
+                    y.parentNode.insertBefore(t, y);
+                }
+            })(window, document, "clarity", "script", "o1li83b01p");
+        }
+    }, []);
+
     return (
         <>
             <QueryClientProvider client={queryClient}>
