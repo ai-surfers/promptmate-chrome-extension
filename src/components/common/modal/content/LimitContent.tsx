773f7b116cd55d4b0bae3439b0cdbc3896f5c2e0
jiwoochris <travelandi01@gmail.com>
2024-11-17 20:02:57 +0900

    플랜 둘러보기 modal 변경

--- src/components/common/modal/content/LimitContent.tsx
+++ src/components/common/modal/content/LimitContent.tsx
@@ -2,41 +2,43 @@ import { Button } from "antd";
 import styled from "styled-components";
 import { openUrlInNewTab } from "../../../../service/chrome/utils";
 
-const defaultText = `안녕하세요! 현재 사용 중인 계정의 개인 프롬프트 저장 한도에 도달했습니다.
+const defaultText = `안녕하세요! 현재 사용 중인 계정의 Private 프롬프트 저장 한도에 도달했습니다.
 
 방금 프롬프트를 삭제하셨다면, 잠시 후 다시 시도해 주세요. 시스템에 반영되는 데 약간의 시간이 필요할 수 있 습니다.
 
-더 많은 개인 아이디어를 저장하고 싶으신가요? 프리미 엄 플랜으로 업그레이드하시면 더 많은 개인 프롬프트를 저장해 사용할 수 있습니다. 당신의 창의력에 날개를 달 아보세요!
+더 많은 Private 프롬프트를 저장하고 싶으신가요? 프리미엄 플랜으로 업그레이드하시면 더 많은 Private 프롬프트를 저장해 사용할 수 있습니다. 당신의 창의력에 날개를 달 아보세요!
 `;
 interface LimitContentProps {
-    text?: string;
+  text?: string;
 }
 export default function LimitContent({
-    text = defaultText,
+  text = defaultText,
 }: LimitContentProps) {
-    return <Text>{text}</Text>;
+  return <Text>{text}</Text>;
 }
 
 const Text = styled.div`
-    white-space: pre-wrap;
-    word-wrap: break-word;
+  white-space: pre-wrap;
+  word-wrap: break-word;
 `;
 
 interface LimitFooterProps {
-    closeModal: () => void;
+  closeModal: () => void;
 }
 export function LimitFooter({ closeModal }: LimitFooterProps) {
-    function handleGoToPlan() {
-        openUrlInNewTab("https://tally.so/r/nW0a0k"); // 폼으로 이동
-        closeModal();
-    }
+  function handleGoToPlan() {
+    openUrlInNewTab(
+      "https://www.pocket-prompt.com/price?utm_source=pocket_prompt_extension&utm_medium=in_app_notification"
+    ); // 폼으로 이동
+    closeModal();
+  }
 
-    return (
-        <>
-            <Button onClick={closeModal}>나중에 하기</Button>
-            <Button type="primary" onClick={handleGoToPlan}>
-                플랜 둘러보기
-            </Button>
-        </>
-    );
+  return (
+    <>
+      <Button onClick={closeModal}>나중에 하기</Button>
+      <Button type="primary" onClick={handleGoToPlan}>
+        플랜 둘러보기
+      </Button>
+    </>
+  );
 }
