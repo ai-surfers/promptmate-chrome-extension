7b8b00cfbfe187bbe30ec939bb6283ad7b6946b0
Yooshin Kim <yusinkim.or@gmail.com>
2024-12-11 22:07:39 +0900

    feat : 즐겨찾기 Limit utm_campaign 추가

--- src/hooks/useErrorModal.tsx
+++ src/hooks/useErrorModal.tsx
@@ -4,20 +4,25 @@ import LimitContent, {
     LimitFooter,
 } from "../components/common/modal/content/LimitContent";
 import eventEmitter, { ErrorArgs, EventType } from "../eventEmitter";
+import { AxiosHeaders } from "axios";
 
 const useErrorModal = () => {
     const { openModal, closeModal } = useModal();
 
     const handleError = (args: ErrorArgs) => {
-        if (args.code === 402) openLimitModal(args.message);
+        if (args.code === 402) openLimitModal(args.message, args.headers);
         else openErrorModal(args.message);
     };
 
-    function openLimitModal(message: string) {
+    function openLimitModal(message: string, headers?: AxiosHeaders) {
+        const utm_campaign = headers?.["utm_campaign"];
+
         openModal({
             title: "개인 프롬프트 저장소가 가득 찼어요!",
             content: <LimitContent text={message} />,
-            footer: <LimitFooter closeModal={closeModal} />,
+            footer: (
+                <LimitFooter closeModal={closeModal} campaign={utm_campaign} />
+            ),
             callback: closeModal,
         });
     }
