e400956404c9a52b1418244cb38ea88cdcf28d97
Yooshin Kim <yusinkim.or@gmail.com>
2025-01-08 00:59:29 +0900

    feat : 프롬프트 삭제하기 기능 추가

--- src/components/prompt/TopBox.tsx
+++ src/components/prompt/TopBox.tsx
@@ -1,9 +1,15 @@
 import styled from "styled-components";
-import { EditOutlined, InfoOutlined } from "@ant-design/icons";
-import { Button, Tooltip } from "antd";
+import { DeleteOutlined, EditOutlined, InfoOutlined } from "@ant-design/icons";
+import { Button, Modal, Tooltip } from "antd";
 import StarButton from "../common/button/StarButton";
 import { useGetUser } from "../../hooks/queries/auth/useGetUser";
 import { openPocketPromptInNewTab } from "../../service/chrome/utils";
+import { useDeletePrompt } from "@/hooks/mutations/prompt/useDeletePrompt";
+import { useAlert } from "@/hooks/useAlert";
+import { useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { PROMPT_KEYS } from "@/hooks/queries/QueryKeys";
+import { useNavigate } from "react-router-dom";
 
 interface TopBoxProps {
     id: string;
@@ -24,7 +30,12 @@ export default function TopBox({
         <InfoBoxWrapper>
             <StarButton id={id} isFavorite={isFavorite} />
             <InfoButton onInformationClick={onInformationClick} />
-            {author === data?.data.nickname && <ModifyButton id={id} />}
+            {author === data?.data.nickname && (
+                <>
+                    <ModifyButton id={id} />
+                    <DeleteButton id={id} />
+                </>
+            )}
         </InfoBoxWrapper>
     );
 }
@@ -66,6 +77,70 @@ export const ModifyButton = ({ id }: ModifyButtonProps) => {
                 icon={<EditOutlined />}
                 onClick={handleOnModify}
             />
+        </Tooltip>
+    );
+};
+
+export const DeleteButton = ({ id }: { id: string }) => {
+    const queryClient = useQueryClient();
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const { openAlert, closeAlert } = useAlert();
+
+    const navigate = useNavigate();
+
+    const { mutate: deletePrompt } = useDeletePrompt({
+        onSuccess(res) {
+            console.log(">>", res);
+
+            const { success, detail } = res;
+
+            if (!success) {
+                console.log(`${detail}`);
+                alert(`${detail}`);
+                return;
+            }
+
+            openAlert({
+                content: id + "가 성공적으로 삭제되었습니다",
+                callback() {
+                    closeAlert();
+                    queryClient.invalidateQueries({
+                        queryKey: PROMPT_KEYS.detail(id),
+                    });
+                    queryClient.invalidateQueries({
+                        queryKey: PROMPT_KEYS.lists(),
+                    });
+
+                    navigate("/", { replace: true });
+                },
+            });
+        },
+        onError(e) {
+            console.log(">");
+        },
+    });
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+        deletePrompt(id);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <Tooltip title="delete">
+            <Button
+                shape="circle"
+                icon={<DeleteOutlined />}
+                onClick={() => setIsModalOpen(true)}
+            />
+
+            <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                정말로 프롬프트를 삭제하시겠습니까?
+            </Modal>
         </Tooltip>
     );
 };
