4c107635d0cac6f8ddb0adc801cede4c41e7137d
Yooshin Kim <yusinkim.or@gmail.com>
2025-01-12 01:26:59 +0900

    chore : 디자인 토큰 적용

--- panda.config.ts
+++ panda.config.ts
@@ -1,11 +1,367 @@
 import { defineConfig } from "@pandacss/dev";
+import { defineTextStyles } from "@pandacss/dev";
+
+export const textStyles = defineTextStyles({
+    xlarge_36_bold: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "2.25rem",
+            fontStyle: "normal",
+            fontWeight: "700",
+            lineHeight: "144%", // 3.24rem
+            letterSpacing: "-0.045rem",
+        },
+    },
+    xlarge_36_regular: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "2.25rem",
+            fontStyle: "normal",
+            fontWeight: "400",
+            lineHeight: "144%", // 3.24rem
+            letterSpacing: "-0.045rem",
+        },
+    },
+    large_32_bold: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "2rem",
+            fontStyle: "normal",
+            fontWeight: "700",
+            lineHeight: "144%", // 2.88rem
+            letterSpacing: "-0.04rem",
+        },
+    },
+    large_32_reg: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "2rem",
+            fontStyle: "normal",
+            fontWeight: "400",
+            lineHeight: "144%", // 2.88rem
+            letterSpacing: "-0.04rem",
+        },
+    },
+    h1_24_bold: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "24px",
+            fontStyle: "normal",
+            fontWeight: "700",
+            lineHeight: "144%", // 34.56px
+            letterSpacing: "-0.48px",
+        },
+    },
+    h1_24_semi: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "24px",
+            fontStyle: "normal",
+            fontWeight: "600",
+            lineHeight: "144%", // 34.56px
+            letterSpacing: "-0.48px",
+        },
+    },
+    h1_24_med: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "24px",
+            fontStyle: "normal",
+            fontWeight: "500",
+            lineHeight: "144%", // 34.56px
+            letterSpacing: "-0.48px",
+        },
+    },
+    h1_24_reg: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "24px",
+            fontStyle: "normal",
+            fontWeight: "400",
+            lineHeight: "144%", // 34.56px
+            letterSpacing: "-0.48px",
+        },
+    },
+    h2_20_bold: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "1.25rem",
+            fontStyle: "normal",
+            fontWeight: "700",
+            lineHeight: "144%", // 28.8px
+            letterSpacing: "-0.4px",
+        },
+    },
+    h2_20_semi: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "1.25rem",
+            fontStyle: "normal",
+            fontWeight: "600",
+            lineHeight: "144%", // 28.8px
+            letterSpacing: "-0.4px",
+        },
+    },
+    h2_20_med: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "1.25rem",
+            fontStyle: "normal",
+            fontWeight: "500",
+            lineHeight: "144%", // 28.8px
+            letterSpacing: "-0.4px",
+        },
+    },
+    h2_20_reg: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "1.25rem",
+            fontStyle: "normal",
+            fontWeight: "400",
+            lineHeight: "144%", // 28.8px
+            letterSpacing: "-0.4px",
+        },
+    },
+    b1_18_bold: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "1.125rem",
+            fontStyle: "normal",
+            fontWeight: "700",
+            lineHeight: "150%", // 1.6875rem
+            letterSpacing: "-0.0225rem",
+        },
+    },
+    b1_18_semi: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "1.125rem",
+            fontStyle: "normal",
+            fontWeight: "600",
+            lineHeight: "150%", // 1.6875rem
+            letterSpacing: "-0.0225rem",
+        },
+    },
+    b1_18_med: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "1.125rem",
+            fontStyle: "normal",
+            fontWeight: "500",
+            lineHeight: "150%", // 1.6875rem
+            letterSpacing: "-0.0225rem",
+        },
+    },
+    b1_18_reg: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "1.125rem",
+            fontStyle: "normal",
+            fontWeight: "400",
+            lineHeight: "150%", // 1.6875rem
+            letterSpacing: "-0.0225rem",
+        },
+    },
+    b2_16_bold: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "1rem",
+            fontStyle: "normal",
+            fontWeight: "700",
+            lineHeight: "150%", // 1.5rem
+        },
+    },
+    b2_16_semi: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "1rem",
+            fontStyle: "normal",
+            fontWeight: "600",
+            lineHeight: "150%", // 1.5rem
+        },
+    },
+    b2_16_med: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "1rem",
+            fontStyle: "normal",
+            fontWeight: "500",
+            lineHeight: "150%", // 1.5rem
+        },
+    },
+    b2_16_reg: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "1rem",
+            fontStyle: "normal",
+            fontWeight: "400",
+            lineHeight: "150%", // 1.5rem
+        },
+    },
+    b3_14_bold: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "0.875rem",
+            fontStyle: "normal",
+            fontWeight: "700",
+            lineHeight: "150%", // 1.3125rem
+            letterSpacing: "-0.0175rem",
+        },
+    },
+    b3_14_semi: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "0.875rem",
+            fontStyle: "normal",
+            fontWeight: "600",
+            lineHeight: "150%", // 1.3125rem
+            letterSpacing: "-0.0175rem",
+        },
+    },
+    b3_14_med: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "0.875rem",
+            fontStyle: "normal",
+            fontWeight: "500",
+            lineHeight: "150%", // 1.3125rem
+            letterSpacing: "-0.0175rem",
+        },
+    },
+    b3_14_reg: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "0.875rem",
+            fontStyle: "normal",
+            fontWeight: "400",
+            lineHeight: "150%", // 1.3125rem
+            letterSpacing: "-0.0175rem",
+        },
+    },
+    c1_12_bold: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "0.75rem",
+            fontStyle: "normal",
+            fontWeight: "700",
+            lineHeight: "150%", // 1.125rem
+        },
+    },
+    c1_12_semi: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "0.75rem",
+            fontStyle: "normal",
+            fontWeight: "600",
+            lineHeight: "150%", // 1.125rem
+        },
+    },
+    c1_12_med: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "0.75rem",
+            fontStyle: "normal",
+            fontWeight: "500",
+            lineHeight: "150%", // 1.125rem
+        },
+    },
+    c1_12_reg: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "0.75rem",
+            fontStyle: "normal",
+            fontWeight: "400",
+            lineHeight: "150%", // 1.125rem
+        },
+    },
+    c2_11_bold: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "0.6875rem",
+            fontStyle: "normal",
+            fontWeight: "700",
+            lineHeight: "150%", // 1.03125rem
+        },
+    },
+    c2_11_semi: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "0.6875rem",
+            fontStyle: "normal",
+            fontWeight: "600",
+            lineHeight: "150%", // 1.03125rem
+        },
+    },
+    c2_11_med: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "0.6875rem",
+            fontStyle: "normal",
+            fontWeight: "500",
+            lineHeight: "150%", // 1.03125rem
+        },
+    },
+    c2_11_reg: {
+        value: {
+            fontFamily: "Pretendard",
+            fontSize: "0.6875rem",
+            fontStyle: "normal",
+            fontWeight: "400",
+            lineHeight: "150%", // 1.03125rem
+        },
+    },
+});
 
 export default defineConfig({
     preflight: true,
+    outdir: "styled-system",
     include: ["./src/**/*.{js,jsx,ts,tsx}"],
     exclude: [],
     theme: {
-        extend: {},
+        extend: {
+            textStyles,
+            tokens: {
+                colors: {
+                    black: { value: "#000000ff" },
+                    white: { value: "#ffffffff" },
+                    primary: {
+                        dark: { value: "#535dbfff" },
+                        normal: { value: "#7580eaff" },
+                        light: { value: "#9eadfcff" },
+                        xlight: { value: "#cedeffff" },
+                        5: { value: "#f8f8feff" },
+                        10: { value: "#f2f3fdff" },
+                        20: { value: "#e3e6fbff" },
+                        30: { value: "#d6d9f9ff" },
+                        40: { value: "#c8ccf7ff" },
+                        50: { value: "#bbc0f5ff" },
+                        60: { value: "#acb3f2ff" },
+                        70: { value: "#9fa7f1ff" },
+                        80: { value: "#9199eeff" },
+                        90: { value: "#838dedff" },
+                        100: { value: "#7580eaff" },
+                    },
+                    gray: {
+                        50: { value: "#f7f8f9ff" },
+                        100: { value: "#f1f2f6ff" },
+                        200: { value: "#dee0e8ff" },
+                        300: { value: "#afb1c1ff" },
+                        400: { value: "#818491ff" },
+                        500: { value: "#5b5f70ff" },
+                        600: { value: "#3e4151ff" },
+                        700: { value: "#2e3040ff" },
+                        800: { value: "#202232ff" },
+                        900: { value: "#181b29ff" },
+                    },
+                },
+                shadows: {
+                    "container-hover": { value: "0 0 64px #7580ea2e" },
+                    "search-hover": { value: "0 0 24px #7580ea4d" },
+                    "btn-hover": { value: "0 2px 16px #7580eaff" },
+                    "image-hover": { value: "0 0 64px #7580ea80" },
+                    "container-big": { value: "0 0 124px #7580ea26" },
+                },
+            },
+        },
     },
-    outdir: "styled-system",
 });
