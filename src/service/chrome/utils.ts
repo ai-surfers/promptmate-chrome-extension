cba136cd8e856278c1eb353f354cfddede0bc0ba
Yooshin Kim <yusinkim.or@gmail.com>
2025-01-12 00:39:07 +0900

    refac : 프롬프트 inject 함수 리팩토링

--- src/service/chrome/utils.ts
+++ src/service/chrome/utils.ts
@@ -10,160 +10,121 @@ import { ACCESS_TOKEN } from "./storage.keys";
 
 /**
  * DOM의 첫번째 인풋에 텍스트 inject & send
- * @param text
+ * @param text 삽입할 텍스트
  */
-export const insertPromptToDOMInput = (text: string) => {
-    // ChatGPT
-    const insertValueToChatGPT = (value: string) => {
-        console.log("value is", value);
-        const triggerSendButton = () => {
-            const sendButton = document.querySelector(
-                '[data-testid="send-button"]'
-            );
-            if (sendButton) {
-                (sendButton as HTMLElement).click();
-            } else {
-                console.log("Send button not found");
+export const insertPromptToDOMInput = (text: string): void => {
+    const handlers: Record<
+        Exclude<AIPlatformType, AIPlatformType.NONE>,
+        (value: string) => void
+    > = {
+        [AIPlatformType.CHATGPT]: (value: string): void => {
+            // [ChatGPT] enter = 줄바꿈 / 버튼 클릭 = 전송
+            const triggerSendButton = (): void => {
+                const sendButton = document.querySelector(
+                    '[data-testid="send-button"]'
+                );
+                if (sendButton) {
+                    (sendButton as HTMLElement).click();
+                } else {
+                    console.log("Send button not found");
+                }
+            };
+
+            const promptTextarea = document.getElementById("prompt-textarea");
+            if (promptTextarea) {
+                const editableDiv = promptTextarea as HTMLElement;
+                editableDiv.innerHTML = value.replace(/\n/g, "<p></p>");
+                setTimeout(triggerSendButton, 100);
+                return;
             }
-        };
 
-        const triggetInputEvent = (element: HTMLElement) => {
-            const inputEvent = new Event("input", {
-                bubbles: true,
-                cancelable: true,
-            });
-            element.dispatchEvent(inputEvent);
-        };
+            const textarea = document.querySelector(
+                "textarea"
+            ) as HTMLTextAreaElement | null;
+            if (textarea) {
+                textarea.value = value;
+                setTimeout(triggerSendButton, 100);
+                return;
+            }
 
-        // ChatGPT - <input>
-        const promptTextarea = document.getElementById("prompt-textarea");
-        console.log("Is promptTextarea?", promptTextarea);
-        if (promptTextarea) {
-            const editableDiv = promptTextarea as HTMLElement;
-            editableDiv.innerHTML = value.replace(/\n/g, "<br>");
+            const contentEditableDivs = document.querySelectorAll(
+                'div[contenteditable="true"]'
+            );
+            if (contentEditableDivs.length > 0) {
+                const editableDiv = contentEditableDivs[0] as HTMLElement;
+                editableDiv.innerHTML = value.replace(/\n/g, "<p></p>");
+                setTimeout(triggerSendButton, 100);
+            }
+        },
+        [AIPlatformType.GEMINI]: (value: string): void => {
+            // [Gemini] enter = 전송 / shift + enter = 줄바꿈
+            const triggerEnterKey = (element: HTMLElement): void => {
+                const event = new KeyboardEvent("keydown", {
+                    bubbles: true,
+                    cancelable: true,
+                    key: "Enter",
+                    code: "Enter",
+                    keyCode: 13,
+                    which: 13,
+                });
+                element.dispatchEvent(event);
+            };
 
-            setTimeout(() => {
-                triggerSendButton();
-            }, 100);
-            return;
-        }
+            const contentEditableDiv = document.querySelector(
+                'div[contenteditable="true"]'
+            ) as HTMLElement | null;
+            if (contentEditableDiv) {
+                contentEditableDiv.innerHTML = value.replace(/\n/g, "<p></p>");
+                triggerEnterKey(contentEditableDiv);
+            }
+        },
+        [AIPlatformType.CLAUDE]: (value: string): void => {
+            // [Claude] enter = 전송 / shift + enter = 줄바꿈
+            const triggerEnterKey = (element: HTMLElement): void => {
+                const event = new KeyboardEvent("keydown", {
+                    bubbles: true,
+                    cancelable: true,
+                    key: "Enter",
+                    code: "Enter",
+                    keyCode: 13,
+                    which: 13,
+                });
+                element.dispatchEvent(event);
+            };
 
-        const inputFields = document.querySelectorAll("textarea");
-        console.log("Is inputFields?", inputFields);
-        if (inputFields.length > 0) {
-            const textarea = inputFields[0] as HTMLTextAreaElement;
-            textarea.value = value;
-
-            const inputEvent = new Event("input", { bubbles: true });
-            textarea.dispatchEvent(inputEvent);
-
-            triggetInputEvent(textarea);
-            triggerSendButton();
-            return;
-        }
-
-        const contentEditableDivs = document.querySelectorAll(
-            'div[contenteditable="true"]'
-        );
-        console.log("Is div[contenteditable]?", contentEditableDivs);
-        if (contentEditableDivs.length > 0) {
-            const editableDiv = contentEditableDivs[0] as HTMLElement;
-            editableDiv.innerHTML = value.replace(/\n/g, "<br>");
-
-            setTimeout(() => {
-                triggerSendButton();
-            }, 100);
-
-            return;
-        }
+            const contentEditableDiv = document.querySelector(
+                'div[contenteditable="true"]'
+            ) as HTMLElement | null;
+            if (contentEditableDiv) {
+                contentEditableDiv.innerHTML = value.replace(/\n/g, "<p></p>");
+                setTimeout(() => triggerEnterKey(contentEditableDiv), 100);
+            }
+        },
     };
 
-    // Gemini
-    const insertValueToGemini = (value: string) => {
-        const triggerEnterKey = (element: HTMLElement) => {
-            const event = new KeyboardEvent("keydown", {
-                bubbles: true,
-                cancelable: true,
-                key: "Enter",
-                code: "Enter",
-                keyCode: 13,
-                which: 13,
-            });
-            element.dispatchEvent(event);
-        };
+    getCurrentTabUrl((url: string): void => {
+        const platform = getAIPlatformType(url);
 
-        // Gemini - <div contenteditable>
-        const contentEditableDivs = document.querySelectorAll(
-            'div[contenteditable="true"]'
-        );
-        if (contentEditableDivs.length > 0) {
-            const editableDiv = contentEditableDivs[0] as HTMLElement;
-            editableDiv.innerHTML = value.replace(/\n/g, " <br>");
-
-            triggerEnterKey(editableDiv);
-        }
-    };
-
-    // Claude
-    const insertValueToClaude = (value: string) => {
-        const triggerEnterKey = (element: HTMLElement) => {
-            const event = new KeyboardEvent("keydown", {
-                bubbles: true,
-                cancelable: true,
-                key: "Enter",
-                code: "Enter",
-                keyCode: 13,
-                which: 13,
-            });
-            element.dispatchEvent(event);
-        };
-
-        // Claude - <div contenteditable>
-        const contentEditableDivs = document.querySelectorAll(
-            'div[contenteditable="true"]'
-        );
-        if (contentEditableDivs.length > 0) {
-            const editableDiv = contentEditableDivs[0] as HTMLElement;
-            editableDiv.innerHTML = value.replace(/\n/g, "<p></p>");
-
-            setTimeout(() => {
-                triggerEnterKey(editableDiv);
-            }, 100);
-        }
-    };
-
-    getCurrentTabUrl((url) => {
-        let func: null | ((value: string) => void) = null;
-
-        const ai_platform = getAIPlatformType(url);
-        if (ai_platform === AIPlatformType.CHATGPT) {
-            func = insertValueToChatGPT;
-        } else if (ai_platform === AIPlatformType.CLAUDE) {
-            func = insertValueToClaude;
-        } else if (ai_platform === AIPlatformType.GEMINI) {
-            func = insertValueToGemini;
-        } else {
+        if (platform === AIPlatformType.NONE) {
             console.log("* 지원하지 않는 플랫폼입니다.");
             return;
         }
 
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            if (!func) {
-                console.log("* 함수를 지원하지 않는 플랫폼입니다.");
-                return;
-            }
+        chrome.tabs.query(
+            { active: true, currentWindow: true },
+            (tabs): void => {
+                if (!tabs[0]?.id) {
+                    console.log("* 처리할 탭이 없습니다.");
+                    return;
+                }
 
-            if (!tabs[0].id) {
-                console.log("* 처리할 탭이 없습니다. ");
-                return;
+                chrome.scripting.executeScript({
+                    target: { tabId: tabs[0].id },
+                    func: handlers[platform],
+                    args: [text],
+                });
             }
-
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                func: func,
-                args: [text],
-            });
-        });
+        );
     });
 };
 
