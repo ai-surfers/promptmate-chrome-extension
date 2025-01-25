8a85ffb84df35dc6064681607e7f981c0692d253
Yooshin Kim <yusinkim.or@gmail.com>
2025-01-26 01:45:04 +0900

    style : 프롬프트 등록 버튼 추가

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -7,6 +7,8 @@ import { useState } from 'react';
 import { openPocketPromptInNewTab } from '../../service/chrome/utils';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import Header from '@/components/common/header/Header';
+import { Button } from '@/components/ui/button';
+import { ArrowRight2 } from 'iconsax-react';
 
 export default function HomePage() {
 	const [showVOC, setShowVOC] = useState(false);
@@ -20,38 +22,61 @@ export default function HomePage() {
 		<List type="my" />,
 	];
 
+	return (
+		<div>
+			<Header />
+
+			<div className="relative">
+				<Tabs value={tab} className="relative">
+					<TabsList className="sticky top-[60px] z-10 bg-white w-full">
+						{Object.entries(TabList).map(([key, value]) => (
+							<TabsTrigger
+								key={key}
+								value={key}
+								onClick={() => handleOnChangeTab(key)}
+								className=""
+							>
+								{value}
+							</TabsTrigger>
+						))}
+					</TabsList>
+					{Object.entries(TabList).map(([key, value], idx) => (
+						<TabsContent value={key} className="py-4 px-5 min-h-[100vh]">
+							{components[idx]}
+						</TabsContent>
+					))}
+				</Tabs>
+
+				<FloatButton
+					shape="circle"
+					type="primary"
+					style={{ right: 24 }}
+					icon={<CustomerServiceOutlined />}
+					onClick={() => setShowVOC(true)}
+				/>
+
+				<VOCModal isOpen={showVOC} closeModal={() => setShowVOC(false)} />
+
+				<PromptNewButton />
+			</div>
+		</div>
+	);
+}
+
+const PromptNewButton = () => {
 	const handleNewPrompt = () => {
 		openPocketPromptInNewTab('prompt-new');
 	};
 
 	return (
-		<>
-			<Header />
-
-			<Tabs value={tab} className="relative">
-				<TabsList className="sticky top-[60px] z-10 bg-white w-full">
-					{Object.entries(TabList).map(([key, value]) => (
-						<TabsTrigger key={key} value={key} onClick={() => handleOnChangeTab(key)} className="">
-							{value}
-						</TabsTrigger>
-					))}
-				</TabsList>
-				{Object.entries(TabList).map(([key, value], idx) => (
-					<TabsContent value={key} className="py-4 px-5">
-						{components[idx]}
-					</TabsContent>
-				))}
-			</Tabs>
-
-			<FloatButton
-				shape="circle"
-				type="primary"
-				style={{ right: 24 }}
-				icon={<CustomerServiceOutlined />}
-				onClick={() => setShowVOC(true)}
-			/>
-
-			<VOCModal isOpen={showVOC} closeModal={() => setShowVOC(false)} />
-		</>
+		<div
+			className="sticky bottom-0 right-0 left-0 px-5 pt-3 pb-9 bg-white"
+			onClick={handleNewPrompt}
+		>
+			<Button className="b2_16_semi flex items-center w-full">
+				<div className="flex-grow">프롬프트 등록하러 가기</div>
+				<ArrowRight2 />
+			</Button>
+		</div>
 	);
-}
+};
