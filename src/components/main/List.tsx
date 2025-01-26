fc6322af87879145201ebfbb102d480ded6aec40
Yooshin Kim <yusinkim.or@gmail.com>
2025-01-26 21:14:30 +0900

    chore : 리스트 미사용 코드 제거

--- src/components/main/List.tsx
+++ src/components/main/List.tsx
@@ -4,16 +4,11 @@ import styled from 'styled-components';
 import { useGetPromptList } from '../../hooks/queries/prompt/useGetPromptList';
 import { useMemo, useState } from 'react';
 import ListItem from './ListItem';
-import Search from './Search';
-// import dummies from "../../pages/tutorial/dummies.json";
-// import { GetPromptResponse } from "../../hooks/queries/prompt/useGetPrompt";
-// import { useUser } from "../../hooks/useUser";
 import SortSelectBox from '../prompt/SortSelectBox';
 import FilterSelectBox from '../prompt/FilterSelectBox';
 import { SortBy } from '../../core/Prompt';
 import { SmileTwoTone } from '@ant-design/icons';
-
-// const tutorial = dummies.data as GetPromptResponse;
+import Search from './Search';
 
 interface ListProps {
 	type: string;
@@ -21,8 +16,6 @@ interface ListProps {
 }
 export default function List({ type, onChangeTab }: ListProps) {
 	const navigate = useNavigate();
-
-	// const { userData } = useUser();
 
 	const [page, setPage] = useState(1);
 	const [query, setQuery] = useState<string | undefined>();
@@ -50,62 +43,6 @@ export default function List({ type, onChangeTab }: ListProps) {
 	function handleOnClear() {
 		setQuery(undefined);
 	}
-
-	// TODO
-	// const showTutorial = useMemo(
-	//     () => userData?.user?.total_prompt_executions === 0,
-	//     [userData]
-	// );
-
-	// const [isTour, setIsTour] = useState(showTutorial);
-	// const tutorialRef = useRef(null);
-	// const steps: TourProps["steps"] = [
-	//     {
-	//         title: "프롬프트 사용 튜토리얼",
-	//         description: "해당 프롬프트를 클릭해 보세요",
-	//         target: () => tutorialRef.current!!,
-	//         nextButtonProps: {
-	//             onClick: function navigateToTutorialPrompt() {
-	//                 navigate("/prompt/tutorial");
-	//             },
-	//         },
-	//     },
-	// ];
-
-	// if (showTutorial) {
-	//     return (
-	//         <ListContainer>
-	//             <Search onEnter={handleOnEnter} onClear={handleOnClear} />
-
-	//             <FilterContainer>
-	//                 <SortSelectBox
-	//                     onSelect={(value) => {
-	//                         setSortBy(value);
-	//                     }}
-	//                 />
-	//             </FilterContainer>
-
-	//             {/* Tutorial */}
-	//             {showTutorial && (
-	//                 <div ref={tutorialRef}>
-	//                     <ListItem
-	//                         key={"tutorial"}
-	//                         prompt={tutorial}
-	//                         onClick={() => navigate(`/prompt/tutorial`)}
-	//                     />
-	//                 </div>
-	//             )}
-
-	//             {/* Tutorial Tour*/}
-	//             <Tour
-	//                 open={isTour}
-	//                 onClose={() => setIsTour(false)}
-	//                 steps={steps}
-	//                 zIndex={999}
-	//             />
-	//         </ListContainer>
-	//     );
-	// }`
 
 	const EmptyResult = useMemo(() => {
 		const handleOnChangeTab = () => {
