b61ba1293be1b6aac82790084b85a2d2883a1a83
Yooshin Kim <yusinkim.or@gmail.com>
2025-01-28 18:34:03 +0900

    style : 토스트에 progress 추가

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,33 +1,43 @@
-import { useToast } from "@/hooks/use-toast"
+import { useToast } from '@/hooks/use-toast';
 import {
-  Toast,
-  ToastClose,
-  ToastDescription,
-  ToastProvider,
-  ToastTitle,
-  ToastViewport,
-} from "@/components/ui/toast"
+	Toast,
+	ToastClose,
+	ToastDescription,
+	ToastProgress,
+	ToastProvider,
+	ToastTitle,
+	ToastViewport,
+} from '@/components/ui/toast';
+import { cn } from '@/lib/utils';
 
 export function Toaster() {
-  const { toasts } = useToast()
+	const { toasts } = useToast();
 
-  return (
-    <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        )
-      })}
-      <ToastViewport />
-    </ToastProvider>
-  )
+	return (
+		<ToastProvider>
+			{toasts.map(function ({
+				id,
+				title,
+				description,
+				action,
+				duration,
+				showCloseButton,
+				...props
+			}) {
+				return (
+					<Toast key={id} {...props}>
+						<div className={cn('grid gap-1 group', props.variant)}>
+							{title && <ToastTitle>{title}</ToastTitle>}
+							{description && <ToastDescription>{description}</ToastDescription>}
+						</div>
+						{action}
+
+						{showCloseButton && <ToastClose />}
+						{duration && <ToastProgress duration={duration} toastId={id} />}
+					</Toast>
+				);
+			})}
+			<ToastViewport />
+		</ToastProvider>
+	);
 }
